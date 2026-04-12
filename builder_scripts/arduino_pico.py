8249b17fe4130a3a0165bc24f342a06338be370c
Tyler <tylerbabbel@gmail.com>
2026-04-12 03:14:47 -0600

    Trim bundled TinyUSB sources

--- builder_scripts/arduino_pico.py
+++ builder_scripts/arduino_pico.py
@@ -19,6 +19,10 @@ def skip_adafruit_tinyusb_api(node):
     return None
 
 
+def register_skip(pattern):
+    env.AddBuildMiddleware(skip_adafruit_hid, pattern)
+
+
 def before_build():
     subprocess.run(["git", "config", "--global", "core.longpaths", "true"])
 
@@ -38,21 +42,45 @@ def before_build():
         ("FIRMWARE_VERSION", version_name)
     ])
 
-    env.AddBuildMiddleware(
-        skip_adafruit_hid,
-        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/hid/Adafruit_USBD_HID.cpp"
-    )
-    env.AddBuildMiddleware(
-        skip_adafruit_usbd_device,
-        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/Adafruit_USBD_Device.cpp"
-    )
-    env.AddBuildMiddleware(
-        skip_adafruit_usbd_cdc,
-        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/Adafruit_USBD_CDC.cpp"
-    )
-    env.AddBuildMiddleware(
-        skip_adafruit_tinyusb_api,
-        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/Adafruit_TinyUSB_API.cpp"
-    )
+    skip_patterns = [
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/hid/Adafruit_USBD_HID.cpp",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/Adafruit_USBD_Device.cpp",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/Adafruit_USBD_CDC.cpp",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/Adafruit_TinyUSB_API.cpp",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/Adafruit_USBH_Host.cpp",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/cdc/Adafruit_USBH_CDC.cpp",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/midi/Adafruit_USBD_MIDI.cpp",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/msc/Adafruit_USBD_MSC.cpp",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/msc/Adafruit_USBH_MSC.cpp",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/webusb/Adafruit_USBD_WebUSB.cpp",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/ports/esp32/Adafruit_TinyUSB_esp32.cpp",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/ports/nrf/Adafruit_TinyUSB_nrf.cpp",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/arduino/ports/samd/Adafruit_TinyUSB_samd.cpp",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/class/audio/audio_device.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/class/bth/bth_device.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/class/cdc/cdc_host.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/class/dfu/dfu_device.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/class/dfu/dfu_rt_device.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/class/hid/hid_host.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/class/midi/midi_device.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/class/msc/msc_device.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/class/msc/msc_host.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/class/net/ecm_rndis_device.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/class/net/ncm_device.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/class/usbtmc/usbtmc_device.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/class/vendor/vendor_device.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/class/video/video_device.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/host/hub.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/host/usbh.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/portable/analog/max3421/hcd_max3421.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/portable/microchip/samd/dcd_samd.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/portable/nordic/nrf5x/dcd_nrf5x.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/portable/raspberrypi/pio_usb/dcd_pio_usb.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/portable/raspberrypi/pio_usb/hcd_pio_usb.c",
+        "*framework-arduinopico/libraries/Adafruit_TinyUSB_Arduino/src/portable/raspberrypi/rp2040/hcd_rp2040.c",
+    ]
+
+    for pattern in skip_patterns:
+        env.AddBuildMiddleware(skip_adafruit_hid, pattern)
 
 before_build()
